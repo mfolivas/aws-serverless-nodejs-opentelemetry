535db7baf90e9aee2a99a97133102bd865055763
Marcelo Olivas <mfolivas@gmail.com>
2021-03-22 12:55:13 -0400

    remove metrics that are not being used

--- src/handler.js
+++ src/handler.js
@@ -9,8 +9,8 @@ const api = require('@opentelemetry/api');
 module.exports.hello = async (event) => {
   console.log('Calling counter')
   const requestStartTime = new Date().getMilliseconds();
-  // const traceID = returnTraceIdJson()
-  // console.log('tracerId', traceID)
+  const traceID = returnTraceIdJson()
+  console.log('tracerId', traceID)
   const statusCode = 200
   meter.emitsPayloadMetric(mimicPayLoadSize(), '/outgoing-http-call', statusCode);
   meter.emitReturnTimeMetric(new Date().getMilliseconds() - requestStartTime, '/outgoing-http-call', statusCode);
